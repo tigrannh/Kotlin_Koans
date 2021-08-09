59da100415bb93d8f7627c800ab3c92cbf4f2b91
lenovo <tigo.hakobyan.l@gmail.com>
2021-08-08 23:45:11 -0700

    a

--- Introduction/Default arguments/src/Task.kt
+++ Introduction/Default arguments/src/Task.kt
@@ -5,5 +5,5 @@ fun useFoo() = listOf(
         foo("a"),
         foo("b", number = 1),
         foo("c", toUpperCase = true),
+        foo(name = "d", number = 2, toUpperCase = true)
 )
-foo(name = "d", number = 2, toUpperCase = true)
