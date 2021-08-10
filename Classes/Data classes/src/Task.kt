36b0b5589b1cfb489e04479923f6d980d93ace5a
lenovo <tigo.hakobyan.l@gmail.com>
2021-08-10 16:28:13 -0700

    a

--- Classes/Data classes/src/Task.kt
+++ Classes/Data classes/src/Task.kt
@@ -1,4 +1,5 @@
-class Person
+data class Person(private val name: String, private val age: Int)
+//data class Person(val name: String, val age: Int)
 
 fun getPeople(): List<Person> {
     return listOf(Person("Alice", 29), Person("Bob", 31))
