ee2f96c386107245164bb5e05346c88d7e4b67b9
lenovo <tigo.hakobyan.l@gmail.com>
2021-08-23 10:29:10 -0700

    a

--- Collections/Associate/src/Task.kt
+++ Collections/Associate/src/Task.kt
@@ -1,11 +1,11 @@
 // Build a map from the customer name to the customer
 fun Shop.nameToCustomerMap(): Map<String, Customer> =
-        TODO()
+        customers.associateBy { it.name }
 
 // Build a map from the customer to their city
 fun Shop.customerToCityMap(): Map<Customer, City> =
-        TODO()
+        customers.associateWith { it.city }
 
 // Build a map from the customer name to their city
 fun Shop.customerNameToCityMap(): Map<String, City> =
-        TODO()
+        customers.associateBy(keySelector = Customer::name, valueTransform = Customer::city)
--- Collections/Fold/src/Task.kt
+++ Collections/Fold/src/Task.kt
@@ -1,7 +1,15 @@
 // Return the set of products that were ordered by all customers
 fun Shop.getProductsOrderedByAll(): Set<Product> {
-    TODO()
+    val products = customers.flatMap { it.getOrderedProducts() }.toMutableSet()
+    return customers.fold(products) { prod, cust ->
+            val customerProd = cust.getOrderedProducts()
+            for (i in prod) {
+                if (!customerProd.contains(i)) prod.remove(i)
+            }
+            prod
+
+    }
 }
 
 fun Customer.getOrderedProducts(): List<Product> =
-        TODO()
+    orders.flatMap { it.products }
